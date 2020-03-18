d148c73d3d1094949ac3002b09da7b4ca47696c3
poikilos <7557867+poikilos@users.noreply.github.com>
2020-03-18 18:00:52 -0400

    from mob_pack-v.01a.zip

---
+++ drillers/driller.lua
@@ -0,0 +1,54 @@
+drillers:register_mob ("drillers:driller", {
+	type = "monster",
+	passive = false,
+	damage = 1,
+	attack_type = "dogfight",
+	attacks_monsters = false,
+	hp_min = 50,
+	hp_max = 45,
+	armor = 125,
+	collisionbox = { -0.3, -1.0, -0.3, 0.3, 0.8, 0.3 },
+	visual = "mesh",
+	mesh = "character.b3d",
+	drawtype = "front",
+	textures = {
+		{"driller.png"},
+	},
+	makes_footstep_sound = true,
+        sounds = {
+                
+        },
+	walk_velocity = 7,
+	run_velocity = 10,
+	jump = true,
+	drops = {
+		{name = "default:steel_ingot",
+		chance = 10, min = 1, max = 45},
+	},
+	water_damage = 25,
+	lava_damage = 0,
+	light_damage = 0,
+	follow = {
+	    "drillers:driller" ,
+	},
+	view_range = 12,
+	owner = "",
+	order = "follow",
+	animation = {
+		speed_normal = 15,
+		speed_run = 30,
+		stand_start = 0,
+		stand_end = 79,
+		walk_start = 168,
+		walk_end = 187,
+		run_start = 168,
+		run_end = 187,
+		punch_start = 200,
+		punch_end = 219,
+	},
+	
+})
+
+
+
+drillers:register_egg("drillers:driller", "Driller ", "spawneggs_driller.bmp", 1)
---
+++ drillers/init.lua
@@ -0,0 +1,6 @@
+---Yes, Tails if you are reading this, just keep this mod simple!---
+print("++++++++ Drillers Mod: Code And Some Art By MineYoshi, Idea By Tails The Mese Miner  ENJOY! +++++++++")
+
+local path = minetest.get_modpath("drillers")
+dofile(path.. "/api.lua") 
+dofile(path.. "/driller.lua") 
